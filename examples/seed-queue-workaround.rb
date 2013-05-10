e91326225ff2f7a566d1e55e6ecc6ae65bfbea70
Oded Arbel <oded@geek.co.il>
2013-05-10 18:55:59 +0300

    more documentation for workaround

--- examples/seed-queue-workaround.rb
+++ examples/seed-queue-workaround.rb
@@ -7,4 +7,8 @@ bt = TransmissionRemote.new('http://transmission:transmission@localhost:9091/tra
 # workaround seed queue problem (bug #4617)
 $seed_queue_size = 6
 
-bt.seeding.sort {|a,b| a["queuePosition"] <=> b["queuePosition"]}.select {|t| t["status"] == "seed" }.drop($seed_queue_size).each{|t| t.pause; t.start;}
+bt.
+  seeding.sort {|a,b| a["queuePosition"] <=> b["queuePosition"]}. # list all seeds by queue order
+  select {|t| t["status"] == "seed" }. # "seeding" lists both 'seed' and 'seed-wait', so select only non-queued seeds
+  drop($seed_queue_size). # skip all torrents that should not be queued
+  each{|t| t.pause; t.start;} # restart all seeds that should be queued - as described in the manual workaround for bug #4617
